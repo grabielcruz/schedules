ba336378efebe836a8812336dec4fb9677b160df
Luis Cruz <luisgrabielcruz@gmail.com>
2021-08-09 22:57:05 -0400

    beginning

---
+++ src/types.ts
@@ -0,0 +1,53 @@
+export interface Grade {
+  Name: string;
+  Subjects: Subject[];
+  HoursPerDay: HoursPerDayOfSchedule;
+}
+
+interface HoursPerDayOfSchedule {
+  Monday: DayWeek;
+  Tuesday: DayWeek;
+  Wednesday: DayWeek;
+  Thursday: DayWeek;
+  Friday: DayWeek;
+  Saturday: DayWeek;
+  Sunday: DayWeek;
+}
+
+interface Subject {
+  Name: string;
+  HoursPerWeek: number;
+  MaxHoursPerDay: number;
+  NumOfSharedHours: number;
+  Teacher: Teacher;
+}
+
+interface Teacher {
+  Name: string;
+  Schedule: TeacherSchedule;
+}
+
+interface TeacherSchedule {
+  Monday: DayWeek;
+  Tuesday: DayWeek;
+  Wednesday: DayWeek;
+  Thursday: DayWeek;
+  Friday: DayWeek;
+  Saturday: DayWeek;
+  Sunday: DayWeek;
+}
+
+interface DayWeek {
+  NumOfHours: number;
+  Hours: ScheduleHour[] | TeacherHour[];
+}
+
+interface ScheduleHour {
+  Type: 'free' | 'shared' | 'single' | 'unoccupied';
+}
+
+interface TeacherHour {
+  Type: 'available' | 'unavailable';
+}
+
+type MinutesPerHour = number;
